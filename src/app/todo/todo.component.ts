beeaf487aa7bc09de3de5349ed508f9b4e171240
rakeshpagi <rakesh_prog@yahoo.co.in>
2023-02-10 16:26:02 +0530

    Intro - App

--- src/app/todo/todo.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'hello-todo',
-  templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class TodoComponent {}
